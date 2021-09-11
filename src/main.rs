556f876046646bf963d51d9d5d1c6941880014c4
Sebastian Schlecht <sebastian.schlecht@sparkteams.de>
2021-09-11 21:48:29 +0200

    Added println! macro for Writer

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
 #![no_std]
 #![no_main]
 //#![feature(asm)]
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World";
 
 #[no_mangle]
 pub extern "C" fn _start() {
-    let vga_buffer = 0xb8000 as *mut u8;
+    /*use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), "some numbers {} and {}", 42, 1.337).unwrap();*/
 
-    for (i, &byte)  in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello");
+    println!("World");
 
+    panic!("Some panic message");
     loop {}
 }
 
+
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
