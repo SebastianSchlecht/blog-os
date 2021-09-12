78fc560d8306dc4f0bff461182087c302b106ab8
Sebastian Schlecht <sebastian.schlecht@sparkteams.de>
2021-09-12 13:36:21 +0200

    Added testing framework

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 //#![feature(asm)]
 mod vga_buffer;
+mod serial;
 
 use core::panic::PanicInfo;
 
 
 #[no_mangle]
 pub extern "C" fn _start() {
-    /*use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), "some numbers {} and {}", 42, 1.337).unwrap();*/
+    #[cfg(test)] //Only compile in test build (not available in general build)
+    test_main(); //This function will not return
 
     println!("Hello");
     println!("World");
@@ -20,9 +23,70 @@ pub extern "C" fn _start() {
 }
 
 
-
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+
+/* ##################################
+   #        Test setup code         #
+   ##################################
+ */
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_print!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failure);
+    loop {}
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+    where T: Fn(),
+{
+    fn run(&self) -> () {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[OK]");
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Testable]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(i32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failure = 0x11
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        // Creates am IO port and writes to it. The IO port is created by QEMU (See package.metadata.bootimage in Cargo.toml)
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
