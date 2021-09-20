33d8c625ead2b2bfb62e2ed92e0505225580bed2
Sebastian Schlecht <sebastian.schlecht@sparkteams.de>
2021-09-20 20:11:54 +0200

    added simple keycode support

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -15,6 +15,7 @@ pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(unsafe { ChainedPic
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET + 0,
+    PS2Keyboard = PIC_1_OFFSET + 1,
 }
 
 impl InterruptIndex {
@@ -37,6 +38,8 @@ lazy_static! {
         }
         idt[InterruptIndex::Timer.as_usize()]
             .set_handler_fn(timer_interrupt_handler);
+        idt[InterruptIndex::PS2Keyboard.as_usize()]
+            .set_handler_fn(keyboard_interrupt_handler);
 
         idt
     };
@@ -60,6 +63,38 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFr
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
+}
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    use x86_64::instructions::port::Port;
+    use spin::Mutex;
+    use pc_keyboard::{layouts, DecodedKey, HandleControl,  Keyboard, ScancodeSet1};
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Uk105Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(layouts::Uk105Key, ScancodeSet1,
+                HandleControl::Ignore)
+            );
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60); //PS2 data port
+    let scancode: u8 = unsafe { port.read() };
+
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
+    }
+
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::PS2Keyboard.as_u8());
     }
 }
 
